f7414080bee42fdc77edb03e874ada48c6aa8c94
lauirvin <lauirvin98@gmail.com>
2019-09-19 15:00:46 +0800

    Initial commit

---
+++ src/utilities/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDSTRAa_JuAPCItepylglVrGvUZMGtlGtk",
+  authDomain: "fb-authentication-boilerplate.firebaseapp.com",
+  databaseURL: "https://fb-authentication-boilerplate.firebaseio.com",
+  projectId: "fb-authentication-boilerplate",
+  storageBucket: "",
+  messagingSenderId: "223378780584",
+  appId: "1:223378780584:web:5cdd63af68738bec0dad9d",
+}
+
+export default firebaseConfig
